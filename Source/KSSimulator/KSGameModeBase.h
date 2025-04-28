d5736e90a90bcbb8a6573fd529cfd0a4985ef726
Seung Jae Lee <synyster120@naver.com>
2025-04-28 11:08:16 +0900

    Money UI

--- Source/KSSimulator/KSGameModeBase.h
+++ Source/KSSimulator/KSGameModeBase.h
@@ -13,5 +13,29 @@ UCLASS()
 class KSSIMULATOR_API AKSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AKSGameModeBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	//UPROPERTY(VisibleAnywhere)
+	TSubclassOf<class UGameMainWidget> MainUIClass;
+
+	//UPROPERTY(VisibleAnywhere)
+	class UGameMainWidget* MainUI;
+
+private:
+	int32 Money = 10000;
+	int32 WorldTime = 0;
+	int32 Level = 1;
+	float Rating = 4.0f;
+
+public:
+	void SetMoney(int32 M);
+	int32 GetMoney();
+	void SetRating(float R);
+	float GetRating();
 };
