9bb70b5fb73d3cb2bd727c9f85f3f0ada29a48f0
Seung Jae Lee <synyster120@naver.com>
2025-04-28 10:32:59 +0900

    Revert "Merge branch 'sj' into develop"
    This reverts commit a1f65b1b47f5e08973ccd1334bca5db22763f74f, reversing
    changes made to 3d2d1ce73dca885f750c9dbffe5efd4730a191e6.

--- Source/KSSimulator/KSGameModeBase.h
+++ Source/KSSimulator/KSGameModeBase.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "InputActionValue.h"
 #include "KSGameModeBase.generated.h"
 
 /**
@@ -15,28 +14,4 @@ class KSSIMULATOR_API AKSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
-public:
-	AKSGameModeBase();
-
-protected:
-	virtual void BeginPlay() override;
-
-protected:
-	//UPROPERTY(VisibleAnywhere)
-	TSubclassOf<class UMainWidget> MainUIClass;
-
-	//UPROPERTY(VisibleAnywhere)
-	class UMainWidget* MainUI;
-
-private:
-	int32 Money = 10000;
-	int32 WorldTime = 0;
-	int32 Level = 1;
-	float Rating = 4.0f;
-
-public:
-	void SetMoney(int32 M);
-	int32 GetMoney();
-	void SetRating(float R);
-	float GetRating();
 };
--- Source/KSSimulator/KSSimulator.Build.cs
+++ Source/KSSimulator/KSSimulator.Build.cs
@@ -8,7 +8,7 @@ public class KSSimulator : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
